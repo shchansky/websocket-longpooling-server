dafdea6d56b55e8afff45dbf9b7bd65ba8c961f4
shchansky <gavrilenko71285@gmail.com>
2023-02-18 21:52:48 +0300

    commit

--- longpooling.js
+++ longpooling.js
@@ -1,6 +1,10 @@
-/** импорт библиотек */
+/** импорт библиотеки express */
 const express = require("express");
-const cors = require("express");
+/** импорт библиотеки cors(необходима для отправки запросов с браузера) */
+const cors = require("cors");
+
+/** импорт стандартного модуля node.js типа events(способ управления событиями node.js), чтобы по какому-то событию возвращался ответ на client */
+const events = require("events");
 
 const PORT = 5000;
 
@@ -8,7 +12,13 @@ const PORT = 5000;
 const app = express();
 
 /** добавление cors-middleware */
-app.use(cors())
+app.use(cors());
+
+/** endpoint для get-запроса: 1-й параметр это маршрут, 2-й параметр это callback кот. будет отрабатывать по 1-му маршруту (параметрами он пр инимает запрос и ответ) */
+app.get("get-message", (req, res) => {});
+
+/** endpoint для post-запроса: 1-й параметр это маршрут, 2-й параметр это callback кот. будет отрабатывать по 1-му маршруту (параметрами он пр инимает запрос и ответ) */
+app.post("new-message", (req, res) => {});
 
 /** слушатель 5000 порта, в случае успешного выполнения в логи будет выводиться сообщение */
 app.listen(PORT, () => console.log(`server start on port ${PORT}`));
